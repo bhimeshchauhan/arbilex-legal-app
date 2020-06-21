6894b4cd6ff525eb5e1b72d1bc6eb3f8e9a3ab14
Bhimesh Chauhan <bhimeshchauhan@esarcbot.local>
2020-06-21 01:00:55 -0400

    updated the api to give back max data

--- server/server/api/api/views.py
+++ server/server/api/api/views.py
@@ -1,29 +1,34 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from collections import Counter
+from collections import Counter, defaultdict
 from datetime import datetime, timedelta
 
 import requests
 
 
 def data_aggregation(data):
-    aggregated_data = {}
+    aggregated_data = defaultdict(dict)
     
     military_service = [d['military_service'] for d in data]
-    aggregated_data['avg_military_service'] = Counter(military_service)
+    aggregated_data['avg_military_service']['data'] = Counter(military_service)
+    aggregated_data['avg_military_service']['max'] = max(Counter(military_service), key=Counter(military_service).get)
 
     is_active = [d['is_active'] for d in data]
-    aggregated_data['avg_is_active'] = Counter(is_active)
+    aggregated_data['avg_is_active']['data'] = Counter(is_active)
+    aggregated_data['avg_is_active']['max'] =  max(Counter(is_active), key=Counter(is_active).get)
 
     nominating_party = [d['nominating_party'] for d in data]
-    aggregated_data['avg_nominating_party'] = Counter(nominating_party)
+    aggregated_data['avg_nominating_party']['data'] = Counter(nominating_party)
+    aggregated_data['avg_nominating_party']['max'] =  max(Counter(nominating_party), key=Counter(nominating_party).get)
 
     law_school = [d['law_school'] for d in data]
-    aggregated_data['avg_law_school'] = Counter(law_school)
+    aggregated_data['avg_law_school']['data'] = Counter(law_school)
+    aggregated_data['avg_law_school']['max'] =  max(Counter(law_school), key=Counter(law_school).get)
 
     state_of_birth = [d['state_of_birth'] for d in data]
-    aggregated_data['avg_state_of_birth'] = Counter(state_of_birth)
+    aggregated_data['avg_state_of_birth']['data'] = Counter(state_of_birth)
+    aggregated_data['avg_state_of_birth']['max'] =  max(Counter(state_of_birth), key=Counter(state_of_birth).get)
 
     for d in data:
         if d['finish_date'] and d['start_date']:
