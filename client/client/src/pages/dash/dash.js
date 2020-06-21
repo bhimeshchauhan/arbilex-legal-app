aa06a20e07daf4e6e3916e8df70cb0e71e90e8ff
Bhimesh Chauhan <bhimeshchauhan@esarcbot.local>
2020-06-21 00:22:01 -0400

    added aggregate card styling

--- client/client/src/pages/dash/dash.js
+++ client/client/src/pages/dash/dash.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import NavBar from '../../components/navbar/navbar';
 import Card from '../../components/card/card';
+import './dash.css';
 
 const DashBoard = (props) => {
   return (
     <div>
         <NavBar title={props.title} />
-        <Card />
+        <div className="division">
+            <div className="division-header">
+                <h3>Aggregate Summary Stats</h3>
+                <button className="details-button">View Details</button>
+            </div>
+            <div className="division-body">
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
+        </div>
     </div>
   );
 };
